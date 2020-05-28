143f2fbe9a76e64c00bc1dc3145792515e0c10f9
praveen10pathak85 <praveen85pathak@gmal.com>
2020-05-28 22:28:58 +0530

    Updates

---
+++ precache-manifest.e1d73943cb1447c7726df647d5eade24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5270e7df274d78ab8cca76ef54ecb6ea",
+    "url": "/react-curd-app/index.html"
+  },
+  {
+    "revision": "30f588e7882259510ff0",
+    "url": "/react-curd-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b16dad30bfeb7ec9760",
+    "url": "/react-curd-app/static/js/2.ef25f143.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-curd-app/static/js/2.ef25f143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f588e7882259510ff0",
+    "url": "/react-curd-app/static/js/main.c2456a48.chunk.js"
+  },
+  {
+    "revision": "43b384d26a5164285ba7",
+    "url": "/react-curd-app/static/js/runtime-main.fb1e930b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-curd-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
